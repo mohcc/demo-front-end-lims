74a94cd9116f28799bc94e1bc2d498c58e29d3b6
Charles <cchigoriwa@gmail.com>
2023-09-06 18:16:28 +0200

    Add table to Patient View

--- src/main/java/zw/gov/mohcc/mrs/demofront/lims/view/PatientView.java
+++ src/main/java/zw/gov/mohcc/mrs/demofront/lims/view/PatientView.java
@@ -4,12 +4,17 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.text.WordUtils;
+import org.vaadin.stefan.table.Table;
+import org.vaadin.stefan.table.TableRow;
 import zw.gov.mohcc.mrs.demofront.lims.service.SampleRepository;
 import zw.gov.mohcc.mrs.fhir.lims.entities.LimsPatient;
 import zw.gov.mohcc.mrs.fhir.lims.entities.Sample;
 
-@Route(value="sample/:clientOrderNumber?/patient/view", layout = MainLayout.class)
+@PageTitle("Patient")
+@Route(value = "sample/:clientOrderNumber?/patient/view", layout = MainLayout.class)
 public class PatientView extends VerticalLayout implements BeforeEnterObserver {
 
     private String clientOrderNumber;
@@ -22,13 +27,37 @@ public class PatientView extends VerticalLayout implements BeforeEnterObserver {
     }
 
     private void postInitComponents() {
-        if (sample != null && patient!=null) {
-            add(new H1(patient.getFirstname()+" "+patient.getSurname()));
+        if (sample != null && patient != null) {
+            TableRow detailsRow;
+            add(new H1(patient.getFirstname() + " " + patient.getSurname()));
+
+            Table table = new Table();
             
-            add(new PatientReadOnlyForm(patient));
+            table.setSizeFull();
+
+            detailsRow = table.addRow();
+            detailsRow.addHeaderCell().setText("Patient");
+            detailsRow.addDataCell().setText(patient.getFirstname() + " " + patient.getSurname());
+            detailsRow.addHeaderCell().setText("Client Patient ID");
+            detailsRow.addDataCell().setText(patient.getClientPatientId());
+
+            detailsRow = table.addRow();
+            detailsRow.addHeaderCell().setText("Gender");
+            detailsRow.addDataCell().setText(patient.getGender() != null ? WordUtils.capitalize(patient.getGender().name()) : "");
+            detailsRow.addHeaderCell().setText("Date of Birth");
+            detailsRow.addDataCell().setText(patient.getBirthDate() != null ? patient.getBirthDate().toString() : "");
+
+            detailsRow = table.addRow();
+            detailsRow.addHeaderCell().setText("Cohort");
+            detailsRow.addDataCell().setText("");
+            detailsRow.addHeaderCell().setText("Phone (mobile)");
+            detailsRow.addDataCell().setText(patient.getPhoneMobile() != null ? patient.getPhoneMobile() : "");
+
+            add(table);
+
         } else {
             add(new H1("Patient not found"));
-            
+
         }
     }
 
